9bbfa78008980faa479b319de703118a434db948
andy <643922878@qq.com>
2018-11-16 17:50:44 +0800

    4-8 React-router4 基础

--- note.md
+++ note.md
@@ -2,3 +2,14 @@
 "transform-decorators-legacy" 不用这个配置，使用下面的两个
 ["@babel/plugin-proposal-decorators", { "legacy": true }],
 ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+
+
+
+
+
+
+<BrowserRouter>里面只能有一个顶级的标签</BrowserRouter>
+
+
+<Route path="/" exact component={App}></Route>
+exact 是完全匹配的非正则
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom'
 import { createStore,applyMiddleware,compose} from 'redux'
 import  thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { BrowserRouter,Route,Link } from 'react-router-dom'
+
 import App from './App'
 import { counter} from './index.redux'
 
@@ -10,11 +12,29 @@ import { counter} from './index.redux'
 const store = createStore(counter,
 	compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension(): f=>f)
 )
+function Erying(){
+	return <h2>二营</h2>
+}
+function Qibinglian(){
+	return <h2>骑兵连</h2>
+}
 
 
 ReactDom.render(
-	(<Provider>
-		<App store={ store} />
+	(<Provider store={ store} >
+		<BrowserRouter>
+			<div>
+				<ul>
+					{/*点击对应的link调到匹配的<Route/> 去*/}
+ 					<li><Link to="/">一营</Link></li>
+					<li><Link to="/erying">二营</Link></li>
+					<li><Link to="qibinglian">骑兵连</Link></li>
+				</ul>
+				<Route path="/" exact component={App}></Route>
+				<Route path="/erying" component={Erying}></Route>
+				<Route path="/qibinglian" component={Qibinglian}></Route>
+			</div>
+		</BrowserRouter>
     </Provider>),
 	document.getElementById('root'))
 
