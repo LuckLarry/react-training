05d828edbbd67bda5a0df72fe801ac026e3aade6
andy <643922878@qq.com>
2018-11-20 23:49:20 +0800

    7-2 完善boss功能信息

--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import {NavBar,InputItem,TextareaItem,Button} from 'antd-mobile'
+import {Redirect} from 'react-router-dom'
+
+
 import AvatarSelector from '../../component/avatarselector/avatarselector'
+import {connect} from 'react-redux'
+import {update } from '../../redux/user.redux'
 
 
+@connect(
+    state=>state.user,
+    {update}
+)
 class BossInfo extends  React.Component{
 
     constructor(props){
@@ -17,8 +26,11 @@ class BossInfo extends  React.Component{
         this.setState({[key]:val})
     }
     render(){
+        const path = this.props.location.pathname
+        const redirect = this.props.redirectTo
         return (
             <div>
+                {redirect&&redirect!==path?<Redirect to={this.props.redirectTo}></Redirect>:null}
                 <NavBar mode="dark">Boos完善信息页面</NavBar>
                 <AvatarSelector selectAvatar={(imagename)=>{
                     this.setState({imagename:imagename})
@@ -36,7 +48,7 @@ class BossInfo extends  React.Component{
 
                 </TextareaItem>
 
-                <Button type='primary'>保存</Button>
+                <Button onClick={()=>{this.props.update(this.state)}} type='primary'>保存</Button>
             </div>
         )
     }
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -2,27 +2,23 @@
 import axios from 'axios'
 import {getRedirectPath} from '../util'
 
-const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
-const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 const LOAD_DATA = 'LOAD_DATA'
+const AUTH_SUCCESS='AUTH_SUCCESS'
 
 const initState = {
     redirectTo:'',
     msg:'',
-    isAuth:false,
     user:'',
     type:''
 }
 
 export  function user(state=initState,action){
     switch (action.type) {
-        case REGISTER_SUCCESS:
-            return {...state,msg:'',redirectTo:getRedirectPath(action.payload),isAuth: true,...action.payload}
+        case AUTH_SUCCESS:
+            return {...state,msg:'',redirectTo:getRedirectPath(action.payload),...action.payload}
         case ERROR_MSG:
-            return {...state,isAuth:false,msg:action.msg}
-        case LOGIN_SUCCESS:
-            return {...state,msg:'',redirectTo:getRedirectPath(action.payload),isAuth: true,...action.payload}
+            return {...state,msg:action.msg}
         case LOAD_DATA:
             return {...state,...action.payload}
         default:
@@ -31,21 +27,16 @@ export  function user(state=initState,action){
     return state
 }
 
+function authSuccess(data){
+    return {type:AUTH_SUCCESS,payload:data}
+}
+
 export function loadData(userinfo) {
     return {type:LOAD_DATA,payload:userinfo}
 }
 
 function errorMsg (msg){
     return {msg,type:ERROR_MSG}
-}
-
-function registerSuccess(data) {
-    return {type:REGISTER_SUCCESS,payload:data}
-
-}
-
-function loginSuccess(data) {
-    return {type:LOGIN_SUCCESS,payload: data}
 }
 
 export function register({user,pwd,repeatpwd,type}) {
@@ -58,7 +49,7 @@ export function register({user,pwd,repeatpwd,type}) {
     return dispatch=>{
         axios.post('/user/register',{user,pwd,type}).then(res=>{
             if(res.status==200 && res.data.code===0){
-                dispatch(registerSuccess({user,pwd,type}))
+                dispatch(authSuccess({user,pwd,type}))
             }else{
                 dispatch(errorMsg(res.data.msg))
             }
@@ -72,9 +63,9 @@ export function login({user,pwd}) {
     }
 
     return dispatch=>{
-        axios.post('/user/login',{user,pwd}).then(res=>{
+        axios.post('/user/update',{user,pwd}).then(res=>{
             if(res.status==200&&res.data.code===0){
-                dispatch(loginSuccess(res.data.data))
+                dispatch(authSuccess(res.data.data))
             }else{
                 dispatch(errorMsg(res.data.msg))
             }
@@ -82,4 +73,18 @@ export function login({user,pwd}) {
     }
 }
 
+
+
+export function update(data){
+    return dispatch=>{
+        axios.post('/user/update',data)
+            .then(res=>{
+                if(res.status==200&&res.data.code===0){
+                    dispatch(authSuccess(res.data.data))
+                }else{
+                    dispatch(errorMsg(res.data.msg))
+                }
+            })
+    }
+}
 
