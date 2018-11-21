0e213013519aeaea5eb13b23073ec7eedbbad1a3
andy <643922878@qq.com>
2018-11-21 17:05:16 +0800

    底部导航栏列表

---
+++ src/component/dashboard/dashboard.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {NavBar} from 'antd-mobile'
+import NavLinkBar from '../navlink/navlink'
+
+
+function Boss(){
+    return <h2>Boss</h2>
+}
+
+function Genius(){
+    return <h2>Genius</h2>
+}
+
+function Msg() {
+    return <h2>Msg</h2>
+}
+
+function User() {
+    return <h2>User</h2>
+}
+@connect(
+    state=>state
+)
+class Dashboard extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        const {pathname} = this.props.location
+        const user = this.props.user
+        console.log("this.props.user",this.props.user)
+        const navList = [
+            {
+                path:'/boss',
+                text:'牛人',
+                icon:'boss',
+                title:'牛人列表',
+                component:Boss,
+                hide:user.type =='genius'
+            },
+
+            {
+                path:'/genius',
+                text:'boss',
+                icon:'job',
+                title:'BOSS列表',
+                component:Genius,
+                hide:user.type =='genius'
+            }
+            ,
+            {
+                path:'/msg',
+                text:'消息',
+                icon:'msg',
+                title:'消息列表',
+                component:Msg,
+            },
+            {
+                path:'/me',
+                text:'我',
+                icon:'user',
+                title:'个人中心',
+                component:User,
+            }
+
+        ]
+        return (
+            <div>
+                <NavBar mode='dard'>{navList.find(v=>v.path==pathname).title}</NavBar>
+                <h2>header</h2>
+                <NavLinkBar data={navList}/>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
--- src/index.js
+++ src/index.js
@@ -12,25 +12,27 @@ import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './component/dashboard/dashboard'
+
 import './index.css'
 
 
 const store = createStore(reducers,
 	compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension(): f=>f)
 )
-function Boss(){
-	return <h2>boss页面</h2>
-}
-
+//boss genius me msg
+//<Route component={Dashboard}/>当Route没有设置path，没有使用<Switch/>时就会所有的组件都显示
 ReactDom.render(
 	(<Provider store={ store} >
 		<BrowserRouter>
 			<div>
+
 			<AuthRoute></AuthRoute>
-                <Route path='/bossinfo' component={BossInfo}></Route>
-                <Route path='/geniusinfo' component={GeniusInfo}></Route>
+			<Route path='/bossinfo' component={BossInfo}></Route>
+			<Route path='/geniusinfo' component={GeniusInfo}></Route>
 			<Route path='/login' component={Login}></Route>
 			<Route path='/register' component={Register}></Route>
+			<Route component={Dashboard}/>
 			</div>
 		</BrowserRouter>
     </Provider>),
